147493551230448f81544a7292a1848b200632c0
anandhusb444 <anandhusb444@gmail.com>
2025-03-13 23:23:40 +0530

    Created a generic respones

---
+++ Respones/GenericRespones.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieApplication.Respones
+{
+    public class GenericRespones<T>
+    {
+        public int statusCode { get; set; }
+        public string message { get; set; }
+        public T data { get; set; }
+        public string error { get; set; }
+
+        //need to check that if i can use the record rather then the class.
+        public GenericRespones(int statusCode, string message,T Data,string error)
+        {
+            statusCode = this.statusCode;
+            message = this.message;
+            Data = this.data;
+            error = this.error;
+        }
+    }
+}

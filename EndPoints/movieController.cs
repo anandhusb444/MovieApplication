5119ae759e66b247e3770bc244fbe04907bff306
anandhusb444 <anandhusb444@gmail.com>
2025-03-18 23:06:12 +0530

    Added Controller

---
+++ EndPoints/movieController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApplication.DTOs;
+using MovieApplication.Respones;
+using MovieApplication.Services;
+
+namespace MovieApplication.EndPoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class movieController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public movieController(IMovieService serverice)
+        {
+            _movieService = serverice;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovies(CreateMovieDTO command)
+        {
+            try
+            {
+                var movie = await _movieService.CreateMoviesAsync(command);
+                var response = new GenericRespones<object>(201, "Movie created successfully", movie, null);
+                return Ok(response);
+
+                //2025-03-11T17:30:00Z
+                //fix the realse date time in genrel no body going to give like this .... ISSUE]
+            }
+            catch (Exception ex)
+            {
+                var errorRespones = new GenericRespones<object>(500, "Interal server error", null, ex.Message);
+                return StatusCode(500, errorRespones);
+            }
+        }
+    }
+}

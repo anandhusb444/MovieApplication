0bf02303212b0bd0fcb8e3b9a653debb53fb447e
anandhusb444 <anandhusb444@gmail.com>
2025-04-04 21:42:15 +0530

    Add users

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApplication.DTOs
+{
+    public class UserDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]  
+        public string userName { get; set; }
+        
+        [Required]
+        public string userEmail { get; set; }
+        [Required]
+        [StringLength(100,MinimumLength = 8)]
+        public string password { get; set; }
+    }
+}
---
+++ Services/IuserServices.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApplication.DTOs;
+using MovieApplication.Models;
+using BCrypt.Net;
+
+
+namespace MovieApplication.Services
+{
+    public interface IuserServices
+    {
+        //Task<IEnumerable<Users>> GetAlluser();
+        //Task<Users> GetUserById(Guid id);
+        Task<bool> UserRegister();
+        //Task<bool> UserLogin();
+        //Task<bool> Block_User();
+        //Task<bool> Unblock_User();
+        //Task<bool> Delete_User();
+    }
+
+    public class UserServies:IuserServices
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public async Task<bool> UserRegister(UserDTO usersDto)
+        {
+            try
+            {
+                var isUserExit = await _dbContext.Users.FirstOrDefaultAsync(u => u.email == usersDto.userEmail);
+
+                if(isUserExit == null)
+                {
+                    var salt = BCrypt.Net.BCrypt.GenerateSalt();
+                    var hashPassword = BCrypt.Net.BCrypt.HashPassword(usersDto.password, salt);
+                    var addUser = new Users() { userName = usersDto.userName, email = usersDto.userEmail, passwordHash = usersDto.password };
+
+                }
+            }
+            catch(Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+
+    }
+
+}

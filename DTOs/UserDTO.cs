d3c8ac422873cbcb8f5b6c89891426233767cacb
anandhusb444 <anandhusb444@gmail.com>
2025-08-11 23:12:31 +0530

    Changed the Class to Record Type in the DTO

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -2,16 +2,11 @@
 
 namespace MovieApplication.DTOs
 {
-    public class UserDTO
-    {
+   
 
-        [Required]  
-        public string userName { get; set; }
-        
-        [Required]
-        public string userEmail { get; set; }
-        [Required]
-        [StringLength(100,MinimumLength = 8)]
-        public string password { get; set; }
-    }
+    public record UserDTO(
+        [Required] string userName,
+        [Required] string userEmail,
+        [StringLength(100,MinimumLength = 8)] string password
+        );
 }
